384d976026ba0755e2f20e928159d6dba8ff1d93
LifeLess210 <61515413+LifeLess210@users.noreply.github.com>
2023-10-24 16:29:56 +0200

    Empty Project

---
+++ src/test/java/com/example/method2023/Method2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.method2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Method2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
